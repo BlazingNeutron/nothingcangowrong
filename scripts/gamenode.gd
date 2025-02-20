d3483a7cc5eef8c1202f6aa0b9eaddbddcae2bc4
BlazingNeutron <steven.armstrong+github@gmail.com>
2025-02-20 18:04:01 -0500

    Tutorial overlay is clickable to remove it

--- scripts/gamenode.gd
+++ scripts/gamenode.gd
@@ -8,9 +8,9 @@ extends Node2D
 func _ready() -> void:
 	Game.connect("overlay_start", _on_overlay_start)
 	Game.connect("wormhole_start", _on_wormhole_start)
-	tutorial_overlay.hide()
+	#tutorial_overlay.hide()
 	wormhole_overlay.hide()
-	tutorial_overlay.connect("overlay_animation_complete", _on_overlay_complete)
+	tutorial_overlay.connect("tutorial_overlay_complete", _on_tutorial_overlay_complete)
 	wormhole_overlay.connect("wormhole_overlay_complete", _on_wormhole_complete)
 
 func _on_overlay_start() -> void:
@@ -22,7 +22,7 @@ func _on_wormhole_start() -> void:
 	wormhole_overlay.show()
 	wormhole_overlay.start_wormhole_overlay()
 
-func _on_overlay_complete() -> void:
+func _on_tutorial_overlay_complete() -> void:
 	tutorial_overlay.hide()
 	get_tree().paused = false
 
--- scripts/tutorial_overlay.gd
+++ scripts/tutorial_overlay.gd
@@ -8,4 +8,8 @@ func start_overlay() -> void:
 	animation_player.play("tutorial-focus")
 
 func overlay_complete() -> void:
-	emit_signal("overlay_animation_complete")
+	emit_signal("tutorial_overlay_complete")
+
+func _on_color_rect_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		overlay_complete()
