e126fb5bf72ea7a3b1afa09315b4d788ab9ec545
BlazingNeutron <steven.armstrong+github@gmail.com>
2025-02-20 09:31:15 -0500

    Added starfield and pulsing console light

--- scripts/tutorial_overlay.gd
+++ scripts/tutorial_overlay.gd
@@ -5,7 +5,7 @@ signal overlay_animation_complete
 @onready var animation_player: AnimationPlayer = $ColorRect/AnimationPlayer
 
 func start_overlay() -> void:
-	animation_player.play("dissolve")
+	animation_player.play("tutorial-focus")
 
 func overlay_complete() -> void:
 	emit_signal("overlay_animation_complete")
