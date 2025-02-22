a8a62a1604e3c73aecd6a9ccccddaf82d8941e8e
BlazingNeutron <steven.armstrong+github@gmail.com>
2025-02-22 09:54:00 -0500

    Buttons after death animation

---
+++ scripts/game_over_explosion.gd
@@ -0,0 +1,19 @@
+extends Node2D
+@onready var control: Control = $Control
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var timer: Timer = $Timer
+
+func _ready() -> void:
+	control.hide()
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	control.show()
+
+func _on_restart_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Game.tscn")
+
+func _on_main_menu_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/MainMenu.tscn")
+
+func _on_timer_timeout() -> void:
+	animated_sprite_2d.play()
